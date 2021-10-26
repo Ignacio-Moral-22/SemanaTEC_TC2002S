bd964e5dc69f41741e6443d1b9a4cb7941546803
IgnacioMoral22 <ignacio.moral.22@outlook.com>
2021-10-26 10:14:31 -0500

    Updated Day_1 folder (added utils.js, changed app.js, installed yargs)

--- Day_1/app.js
+++ Day_1/app.js
@@ -1,2 +1,9 @@
-console.log('Hello, World!');
-console.log('Bienvenido a la Semana Tec');
+// const fs = require('fs')
+const checkUtils = require('./utils.js')
+checkUtils()
+
+// console.log('Hello, World!');
+// console.log('Bienvenido a la Semana Tec');
+
+// fs.writeFileSync('notes.txt', 'Bienvenido a la Semana TEC. Este es el curso de desarrollo web para entornos ligeros.')
+// fs.appendFileSync('notes.txt', " - I'm Xord, your host tonight.")
