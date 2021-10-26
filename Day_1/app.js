fffdb204d1f40139cee15259c2cfb522502804e4
IgnacioMoral22 <ignacio.moral.22@outlook.com>
2021-10-26 10:46:22 -0500

    Added Yargs practice, added notes, installed chalk

--- Day_1/app.js
+++ Day_1/app.js
@@ -1,9 +1,41 @@
-// const fs = require('fs')
-const checkUtils = require('./utils.js')
-checkUtils()
 
+//Testing
 // console.log('Hello, World!');
 // console.log('Bienvenido a la Semana Tec');
 
+//Write Text. Require FS
+// const fs = require('fs')
 // fs.writeFileSync('notes.txt', 'Bienvenido a la Semana TEC. Este es el curso de desarrollo web para entornos ligeros.')
 // fs.appendFileSync('notes.txt', " - I'm Xord, your host tonight.")
+
+//Require utils.js
+// const checkUtils = require('./utils.js')
+// checkUtils()
+
+//Required Yargs
+// const yargs = require('yargs')
+// yargs.version('1.1.0')
+// yargs.command({
+//     command: 'add',
+//     describe: 'Add a new note',
+//     handler: function () {
+//         console.log('Adding a new note!')
+//     }
+// })
+// yargs.command({
+//     command: 'remove',
+//     describe: 'Remove a new note',
+//     handler: function () {
+//         console.log('Removing a note!')
+//     }
+// })
+// const command = process.argv[2]
+// if (command === 'add') {
+//     console.log('Add Argument')
+// } else if (command === 'remove') {
+//     console.log('Remove Argument')
+// }
+// yargs.parse()
+// console.log(yargs.argv)
+
+//Actividad 1
